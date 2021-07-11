84e93450aea31ade12955fbfaa575657165fef31
@sanchezalan97 <sanchezalan95@gmail.com>
2021-07-11 12:35:09 -0500

    Update icons for the map

--- src/Components/Map/index.js
+++ src/Components/Map/index.js
@@ -1,33 +1,14 @@
-import React, { useContext } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 
-import ProvidersContext from '../../Context/ProvidersContext';
+import MarkersList from './MarkersList';
 
 import './Map.scss';
 
 const Map = () => {
-  const { state } = useContext(ProvidersContext);
-
-  let markers = null;
-  if (state.providers ) {
-    const { providers } = state;
-    console.log(providers);
-    markers = providers.map((provider) => {
-      const {id, name,location: { latLng, address: {line} },} = provider;
-      return (
-        <Marker key={`markerid-${id}`} position={[latLng.lat, latLng.lng]}>
-          <Popup>
-            {name}
-            <br />
-            {line.join(' ')}
-          </Popup>
-        </Marker>
-      )
-    });
-  }
-
   return (
+    <>
     <MapContainer center={[37.6000, -95.6650]} zoom={4} scrollWheelZoom={false} className="providers-map">
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -35,9 +16,10 @@ const Map = () => {
       />
 
       <MarkerClusterGroup showCoverageOnHover={false}>
-        {markers}
+        <MarkersList />
       </MarkerClusterGroup>
     </MapContainer>
+    </>
   )
 }
 
