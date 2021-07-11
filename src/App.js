0e2c04bdc327bd6e38a860f33e142fbd6075990a
@sanchezalan97 <sanchezalan95@gmail.com>
2021-07-11 12:56:56 -0500

    Add header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import ProvidersContextProvider from './Providers/ProvidersContextProvider';
 
 import Map from './Components/Map';
+import Header from './Components/Header';
 
 function App() {
   return (
     <ProvidersContextProvider>
+      <Header />
       <Map />
     </ProvidersContextProvider>
   );
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,2 +1,17 @@
 import React from 'react';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className="providers-header">
+            <div className="providers-header__container">
+                <LocalHospitalIcon />
+                <h1 className="providers-header__title">Health Providers</h1>
+            </div>
+        </header>
+    )
+};
+
+export default Header;
